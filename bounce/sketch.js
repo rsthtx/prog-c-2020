2e219974cf14f774c0b8de7bdf88ca5e103edf20
Rasmus Stougaard <rst@eucnordvest.dk>
2020-10-01 23:25:28 +0200

    WIP. Add bounce demo

---
+++ bounce/sketch.js
@@ -0,0 +1,28 @@
+let cx = 50;
+
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+
+  cx = mouseX;
+
+  circle(cx, 300, 50);
+
+  if (cx > width/2) {
+    fill("red");
+  } else {
+    fill("blue");
+  }
+
+  cx = cx + 1;
+
+  // TODO  make it bounce
+
+
+  circle(width, height, 200);
+
+}
