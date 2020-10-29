8699ff7a18eda7435950d7451f1435ed27ba68a9
Rasmus Stougaard <rst@eucnordvest.dk>
2020-10-29 23:00:09 +0100

    Loop 4

--- loops4/sketch.js
+++ loops4/sketch.js
@@ -6,9 +6,16 @@ function setup() {
   // udskriver værdierne af y for x=0, x=1,x= 2, x=3 ... x=10. 
   // Ret det derefter til at skrive ud for x=0,x=10,x=20,x=30...x=100.
 
+  console.log("Første del")
   for(let x = 0; x <= 10; x++){
     let y = 3 * x * x + 6 * x + 9
-    console.log(y);
+    console.log(`x: ${x}, y: ${y}`);
+  }
+  
+  console.log("Anden del")
+  for(let x = 0; x <= 100; x+=10){
+    let y = 3 * x * x + 6 * x + 9
+    console.log(`x: ${x}, y: ${y}`);
   }
 }
 
